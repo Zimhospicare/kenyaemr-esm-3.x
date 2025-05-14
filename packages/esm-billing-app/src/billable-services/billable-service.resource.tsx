3825d6a6d1e6dd64a9ff8f5485c7b6a770e46fc2
abertnamanya <abertnamanya@gmail.com>
2025-05-14 19:52:43 +0300

    Change the serviceUUId

--- packages/esm-billing-app/src/billable-services/billable-service.resource.tsx
+++ packages/esm-billing-app/src/billable-services/billable-service.resource.tsx
@@ -14,7 +14,7 @@ export const useBillableServices = () => {
 
 export function useServiceTypes() {
   // service concept UUID containing all available services e.g lab, pharmacy, surgical etc
-  const serviceConceptUuid = `a8f3f64a-11d5-4a09-b0fb-c8118fa349f3`;
+  const serviceConceptUuid = `0f23183c-a593-4e7d-aa4d-871d7165bb91`;
   const url = `/ws/rest/v1/concept/${serviceConceptUuid}?v=custom:(setMembers:(uuid,display,id))`;
   const { data, error, isLoading } = useSWR<{ data: ServiceTypesResponse }>(url, openmrsFetch, {});
   return { serviceTypes: data?.data.setMembers ?? [], error, isLoading };
